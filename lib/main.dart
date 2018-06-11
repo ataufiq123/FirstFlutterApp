cd65bdc270dc242ffa6c9f97adabc38398da6a48
Ahmad Taufiq Hidayat <ataufiq665@gmail.com>
2018-06-11 15:02:40 +0700

    Create Infinite Scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
         body: new Center(
           // child: const Text('Hello World'),
           //child: new Text(wordPair.asPascalCase),  // With this text.
@@ -26,12 +23,45 @@ class MyApp extends StatelessWidget {
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
+
 }
 
 class RandomWordsState extends State<RandomWords> {
-  @override                                  // Add from this line ... 
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
-  }                                          // ... to this line.
-}
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}                                  
+
